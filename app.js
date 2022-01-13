972a1c6b5e2d61b030066ca9f4b719e37f451f54
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-13 18:46:19 +0100

    Update basic.ts

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hey');
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
+}
+var n1 = 5;
+var n2 = 2;
+var printResult = true;
+var resultPhrase = 'Result is:';
+add(n1, n2, printResult, resultPhrase);
---
+++ basics.ts
@@ -0,0 +1,14 @@
+function add(n1: number,n2: number, showResult: boolean, phrase:string){
+    const result = n1 + n2;
+    if ( showResult){
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
+}
+
+const n1 = 5;
+const n2 = 2;
+const printResult = true;
+const resultPhrase = 'Result is:';
+add(n1,n2,printResult,resultPhrase);
